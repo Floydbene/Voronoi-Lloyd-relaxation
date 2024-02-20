ef5366622ee7493fb9fd1bf6613996ca879f976b
Floyd <floydbenedikter@Floyds-Laptop.attlocal.net>
2024-02-20 16:22:44 -0500

    fixed recency issue

--- src/Components/D3Graph.jsx
+++ src/Components/D3Graph.jsx
@@ -6,7 +6,6 @@ import { useState } from 'react';
 const D3GraphWithDelaunay = ({ pixelArray, width, height, imgData }) => {
   const [displayArray, setDisplayArray] = useState(pixelArray);
   const [which, setWhich] = useState(null);
-  const [loading, setLoading] = useState(true);
   const svgRef = useRef(null);
   const svgWidth = width; // SVG width
   const svgHeight = height; // SVG height
@@ -56,7 +55,6 @@ const D3GraphWithDelaunay = ({ pixelArray, width, height, imgData }) => {
         .style('stroke', '#ccc')
         .style('fill', 'none');
     }
-    setLoading(false);
   }, [displayArray, which]);
 
   const clicky = () => {
@@ -90,57 +88,51 @@ const D3GraphWithDelaunay = ({ pixelArray, width, height, imgData }) => {
     }
     setDisplayArray(relaxedPixels); // Update the state to trigger a re-render
   };
-  useEffect(() => {
-    setWhich('delaunay');
-    reset();
-  }, []);
 
-  if (loading) return <h1>loading...</h1>;
-  else
-    return (
-      <>
-        <div className='canvas'>
-          <svg
-            ref={svgRef}
-            width={width}
-            height={height}
-            className='w-[400px] h-[400px] bg-white border-solide border-4 border-black dark:border-[var(--burnt-sienna)]  mx-[auto]'
-          ></svg>
-        </div>
-        <div className='btns grid gap-1 grid-cols-6 w-[400px] mx-[auto] my-4'>
-          <button
-            className='col-span-3 dark:bg-[var(--burnt-sienna)] text-white'
-            onClick={() => clicky()}
-          >
-            Relax Vertices
-          </button>
-          <button
-            className='col-span-3 dark:bg-[var(--burnt-sienna)] text-white'
-            onClick={() => setWhich('delaunay')}
-          >
-            See Delaunay Triangulation
-          </button>
-          <button
-            className='col-span-3 dark:bg-[var(--burnt-sienna)] text-white'
-            onClick={() => setWhich('voronoi')}
-          >
-            See Voronoi Graph
-          </button>
-          <button
-            className='col-span-3 dark:bg-[var(--burnt-sienna)] text-white'
-            onClick={() => setWhich(null)}
-          >
-            Remove Grid
-          </button>
-          <button
-            className='col-span-6 dark:bg-[var(--burnt-sienna)] text-white'
-            onClick={() => reset()}
-          >
-            Reset Image
-          </button>
-        </div>
-      </>
-    );
+  return (
+    <>
+      <div className='canvas'>
+        <svg
+          ref={svgRef}
+          width={width}
+          height={height}
+          className='w-[400px] h-[400px] bg-white border-solide border-4 border-black dark:border-[var(--burnt-sienna)]  mx-[auto]'
+        ></svg>
+      </div>
+      <div className='btns grid gap-1 grid-cols-6 w-[400px] mx-[auto] my-4'>
+        <button
+          className='col-span-3 dark:bg-[var(--burnt-sienna)] text-white'
+          onClick={() => clicky()}
+        >
+          Relax Vertices
+        </button>
+        <button
+          className='col-span-3 dark:bg-[var(--burnt-sienna)] text-white'
+          onClick={() => setWhich('delaunay')}
+        >
+          See Delaunay Triangulation
+        </button>
+        <button
+          className='col-span-3 dark:bg-[var(--burnt-sienna)] text-white'
+          onClick={() => setWhich('voronoi')}
+        >
+          See Voronoi Graph
+        </button>
+        <button
+          className='col-span-3 dark:bg-[var(--burnt-sienna)] text-white'
+          onClick={() => setWhich(null)}
+        >
+          Remove Grid
+        </button>
+        <button
+          className='col-span-6 dark:bg-[var(--burnt-sienna)] text-white'
+          onClick={() => reset()}
+        >
+          Reset Image
+        </button>
+      </div>
+    </>
+  );
 };
 
 export default D3GraphWithDelaunay;
